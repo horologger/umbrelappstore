4dcda6f07470153974d2e173ad812d82417b634f
horologger <horologger@protonmail.com>
2025-03-21 19:35:30 -0400

    APP_SPACES_FABRIC_PORT=22254

--- isviable-spaces/exports.sh
+++ isviable-spaces/exports.sh
@@ -1,3 +1,3 @@
 export APP_SPACES_FABRIC_HOST='mainnet.fabric.nostrops.space'
 export APP_SPACES_FABRIC_IPV4="70.251.209.207"
-export APP_SPACES_FABRIC_PORT="22253"
+export APP_SPACES_FABRIC_PORT="22254"
